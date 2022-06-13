352017e0598fca5ece081bfc573002517bbd8f37
podmar <marta.podziewska@gmail.com>
2022-06-13 17:19:50 +0200

    imported cors, applied express.json() and express.urlencoded

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
 const port = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(cors());
+
 app.listen(port, () => {
     console.log(`Server is running on the ${port} port.`); 
 });
