ba6ac098426fa4a65c36d9c10deb7f0ecb48174c
podmar <marta.podziewska@gmail.com>
2022-06-14 13:31:03 +0200

    connected to mongoDB with mongoose

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
 import users from "./routes/users.js"
+import * as dotenv from "dotenv";
+import mongoose from "mongoose"; 
+
+// leading .env file
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -13,6 +18,11 @@ app.use(
 );
 app.use(cors());
 app.use('/users', users);
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Connection to MongoDB established"))
+    .catch(err => console.log(err));
 
 app.listen(port, () => {
     console.log(`Server is running on the ${port} port.`); 
