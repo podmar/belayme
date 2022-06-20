d798f8b45153f6d0b3d3b8791dbf69de3e4a16d3
podmar <marta.podziewska@gmail.com>
2022-06-20 12:02:57 +0200

    renamed the route file for Users

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get('/test', (req, res) => {
-    res.send("Test route.");
+    res.send({msg: "Test route."});
 });
 
-export default router;
+export default router
