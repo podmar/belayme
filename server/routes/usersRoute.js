481a2c1c73cd783a94bed2cb03b1e60f6f77978b
podmar <marta.podziewska@gmail.com>
2022-06-20 12:19:48 +0200

    created a draft of the route to fetch all users

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,9 +1,19 @@
 import express from "express";
+import User from "../model/userModel";
 
 const router = express.Router();
 
 router.get('/test', (req, res) => {
     res.send({msg: "Test route."});
 });
+
+router.get("/all", (req, res) => {
+    User.find({}, function(err, users) {
+        if (err) {
+            res.send(`There was a problem: ${err}`);
+        }
+    })
+});
+
 
 export default router
