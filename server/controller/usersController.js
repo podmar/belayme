361c888d03231fb27b9af209005db5a7ebe2e23b
podmar <marta.podziewska@gmail.com>
2022-06-21 12:31:09 +0200

    removed redundant comments and console.log statements

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -1,7 +1,6 @@
 import userModel from "../model/userModel.js"
 
 const getAllUsers = async (req, res) => {
-    // console.log("request: ", req);
     try {
         const allUsers = await userModel.find({})
         if (allUsers.length === 0) {
@@ -21,19 +20,9 @@ const getAllUsers = async (req, res) => {
 };
 
 const getUsersByCurrentLocation = async (req, res) => {
-    // console.log(req)
     try {
-        console.log("hey")
         const usersByLocation = await userModel
         .find({ "current.current_location": req.params.current_location})
-
-        // .find({ current: {current_location: req.params.current_location}})
-        // .find({ current: {$elemMatch: {current_location: req.params.current_location}}})
-
-        // .find({ current: {$elemMatch: {status: { $elemMatch: {current_location: req.params.current_location}}}}})
-        // .find({ current.status.current_location: req.params.current_location}}})
-
-        // .find({ nickname: req.params.current_location})
         .exec()
         console.log("userByLocation", usersByLocation)
         res
