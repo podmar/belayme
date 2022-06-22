01f098ffc9a736092b9858c3cdf631912c75316e
podmar <marta.podziewska@gmail.com>
2022-06-22 16:53:55 +0200

    created a custom theme for mui

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,53 @@ import React from 'react'
 import './App.css';
 import "@fontsource/roboto"
 import {Button, Container} from "@mui/material"
+// import { Theme } from './components/Theme';
+import { ThemeProvider, createTheme } from "@mui/material/styles"
 
 function App() {
+  // const belaymeTheme = createTheme({
+  //   status: {
+  //     background: "linear-gradient(45deg, #F4D03F, #45B39D"
+  //   }
+  // })
+
+  const theme = createTheme ({
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#117A65',
+      },
+      secondary: {
+        main: '#f1c40f',
+      },
+      error: {
+        main: '#c94b60',
+      },
+      background: {
+        default: '#eeeeee',
+        paper: '#e0e4e6',
+      },
+    },
+    shape: {
+      borderRadius: 20,
+    },
+  });
+
   return (
-    <Container maxWidth="md">
-      <div className="App">
-        <header>
-          
-        </header>
-        <Button>
-          Hey I am button
-        </Button>
-      </div>
-    </Container>
+    <ThemeProvider theme= {theme}>
+      <Container maxWidth="md">
+        <div className="App">
+          <header>
+            
+          </header>
+          <Button 
+            variant="contained"
+            color="secondary">
+            Find a belayer
+          </Button>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 }
 
