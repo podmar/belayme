4dc2ce4548facaba2ed31d9d9e4b43fc4326a349
podmar <marta.podziewska@gmail.com>
2022-06-23 14:36:21 +0200

    created the view

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,8 +1,83 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button, Typography } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+      <Typography variant='h2'>
+        Login
+      </Typography>
+      <Typography variant="subtitle1">
+        to find a belayer
+      </Typography>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+          flexDirection: 'column',
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <TextField
+            required
+            size='small'
+            id="nickname-input"
+            label="Nickname"
+            defaultValue="lucy"
+          />
+        </div>
+        <div>
+          <TextField
+            // error
+            // helperText="Invalid email address."
+            required
+            size='small'
+            id="email-input"
+            label="E-mail"
+            type="email"
+            defaultValue="lucy@email.com"
+          />
+        </div>
+        <div>
+          <TextField
+            // fullWidth
+            size='small'
+            id="password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+          />
+        </div>
+        <div>
+          <Button
+            type='submit'
+            endIcon={<SendIcon />}
+            variant='contained'
+          >
+            Login
+          </Button>
+        </div>
+      </Box>
+      <Box
+        sx={{
+          flexDirection: 'column',
+        }}
+      >
+        <div>
+          <p>No account yet? </p>
+        </div>
+        <div>
+          <Button>
+            Go to registration page
+          </Button>
+        </div>
+      </Box>
+    </>  
   )
 }
 
