7ef89072b69a3d581aa20aaa2ad958f8223b7558
podmar <marta.podziewska@gmail.com>
2022-06-24 15:36:42 +0200

    created a context for the fetched climbers

---
+++ client/src/context/ClimbersContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+export const ClimbersContext = createContext();
+
+export const ClimbersContextProvider = (props) => {
+  
+  const [climbers, setClimbers] = useState(null);
+  const [climberCount, setClimberCount] = useState([0]);
+    
+  //TODO change this to get user by location based on current location
+  const url = "http://localhost:5001/users/all"
+
+  const fetchClimbers = async () => {
+      try {
+          const response = await fetch(url);
+          const climbersData = await response.json();
+          console.log(climbersData);
+          setClimbers(climbersData.allUsers);
+          setClimberCount(climbersData.results);
+      } catch (error) {
+          console.log("cannot fetch climbers", error);
+      }
+  };
+
+  return (
+    <ClimbersContext.Provider
+    value = {{climbers, setClimbers}}
+    >
+      {props.children}
+    </ClimbersContext.Provider>
+  )
+}
+
+export default ClimbersContext
