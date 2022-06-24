a34f34fb9f153e24b98a8a0cac626ef27beda233
podmar <marta.podziewska@gmail.com>
2022-06-24 14:09:13 +0200

    displayed title and the number of belayers

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,7 +1,9 @@
+import { Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 
 function List() {
     const [belayers, setBelayers] = useState([]);
+    const [belayerCount, setBelayerCount] = useState([0]);
     
     //TODO change this to get user by location based on current location
     const url = "http://localhost:5001/users/all"
@@ -10,9 +12,10 @@ function List() {
         try {
             const response = await fetch(url);
             const belayersData = await response.json();
-            console.log(belayersData)
-            // setBelayers()
-            
+            console.log(belayersData);
+            setBelayers(belayersData.allUsers);
+            setBelayerCount(belayersData.results);
+            // console.log(belayers, belayerCount);
         } catch (error) {
             console.log("cannot fetch belayers", error);
         }
@@ -23,7 +26,17 @@ function List() {
     }, [])
     
   return (
-    <div>List</div>
+    <div>
+        <Typography variant='h2'>
+            Climbers nearby
+        </Typography>
+        {belayerCount ? 
+            <p>{`Found ${belayerCount} people nearby you.`}</p> :
+            <p>Looking for people nearby...</p>
+        }
+        
+
+    </div>
   )
 }
 
