5800776e6d9ad55a5084b118f2dc1478053312be
podmar <marta.podziewska@gmail.com>
2022-06-24 13:31:11 +0200

    created a bottom navigation bar with react router link component and left the NavBar for possible use later on a big screen

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,8 +8,10 @@ import MenuIcon from '@mui/icons-material/Menu';
 import AddIcon from '@mui/icons-material/Add';
 import SearchIcon from '@mui/icons-material/Search';
 import MoreIcon from '@mui/icons-material/MoreVert';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
+
   return (
     <>
       <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
@@ -17,10 +19,45 @@ function NavBar() {
           <IconButton color="inherit" aria-label="open drawer">
             <MenuIcon />
           </IconButton>
-          {/* <StyledFab color="secondary" aria-label="add">
-            <AddIcon />
-          </StyledFab> */}
-          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ flexGrow: 1 }}>
+            <nav>
+              <ul>
+                <li>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/register">Register</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/login">Login</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/list">List</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/profile">Profile</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/inbox">Inbox</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/belayrequest">Message</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/detail/:userid">Detail</NavLink>
+                </li>
+              </ul>
+            </nav>
+
+
+
+
+
+
+
+
+
+          </Box>
           <IconButton color="inherit">
             <SearchIcon />
           </IconButton>
---
+++ client/src/components/NavBottomWithLinks.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'; 
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import HomeIcon from '@mui/icons-material/Home';
+import ListIcon from '@mui/icons-material/List';
+import PersonIcon from '@mui/icons-material/Person';
+import MailIcon from '@mui/icons-material/Mail';
+import { NavLink, Link } from 'react-router-dom';
+import { Paper } from '@mui/material';
+
+function NavBottomWithLinks() {
+  const [value, setValue] = useState(0);
+
+  return (
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+            setValue(newValue);
+        }}
+        >
+        <BottomNavigationAction
+          component={NavLink} 
+          to="/"
+          label="Home" 
+          activeClassName="current"
+          icon={<HomeIcon />} 
+        />
+        <BottomNavigationAction 
+          component={Link} 
+          to="/list"
+          label="List" 
+          icon={<ListIcon />} 
+        />
+        <BottomNavigationAction 
+          component={Link} 
+          to="/login"
+          label="Login" 
+          icon={<PersonIcon />} 
+        />
+        <BottomNavigationAction 
+          component={Link} 
+          to="/inbox"
+          label="Inbox" 
+          icon={<MailIcon />} 
+        />
+        </BottomNavigation>
+    </Paper>
+  )
+}
+
+export default NavBottomWithLinks;
