4c99ac5550ee5370746a0bee8e1177185cf116de
podmar <marta.podziewska@gmail.com>
2022-06-24 17:27:00 +0200

    wrote the register function and an onChange handler for the inputs

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -4,8 +4,31 @@ import {useNavigate} from 'react-router-dom';
 export const AuthContext = createContext();
 
 export const AuthContextProvider = (props) => {
-    const [user, setUser] = useState(null);
+    const {user, setUser} = useState({});
     const redirectTo = useNavigate(); 
+
+    //1 change handler for 3 fields
+    const handleRegistrationInputChange = (event) => {
+        setUser({...user, [event.target.name]: event.target.value})
+    };
+
+    let urlencoded = new URLSearchParams();
+    urlencoded.append("nickname", user.nickname);
+    urlencoded.append("email", user.email);
+    urlencoded.append("nickname", user.password);
+
+    let requestOptions = {
+        method: "POST",
+        body: urlencoded,
+    };
+
+    const register = async (nickname, email, password) => {
+        try {
+            redirectTo("/list");
+        } catch (error) {
+            console.log("cannot register user", error)
+        }
+    };
 
 
     // const checkIfUserLoggedIn = () => {};
