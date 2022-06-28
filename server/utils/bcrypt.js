b4e98cd9459cf37403a6dd209cdab1af87dc239b
podmar <marta.podziewska@gmail.com>
2022-06-28 11:40:17 +0200

    changed the file name to bcrypt and wrote a function to verify password

---
+++ server/utils/bcrypt.js
@@ -0,0 +1,28 @@
+import bcrypt from "bcrypt"
+
+const encryptPassword = async (password) => {
+    try {
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        return hashPassword;
+    } catch (error) {
+        console.log("Error while hashing the password.", error);
+    }
+};
+
+const verifyPassword = async (password, hashedPassword) => {
+    try {
+        const verificationResult = await bcrypt.compare(password, hashedPassword, function(err, result) {
+        });  
+        return verificationResult;
+    } catch (error) {
+        console.log("Error veryfying password, ". error)
+        return false;
+    }
+};
+
+
+
+export { encryptPassword, verifyPassword };
