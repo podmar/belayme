374096ea783530e4c88747c3563a47af8d4cc74b
podmar <marta.podziewska@gmail.com>
2022-06-29 12:41:21 +0200

    set up a test welcome message reading user from the auth provider

--- client/src/views/Inbox.js
+++ client/src/views/Inbox.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 
 function Inbox() {
+  const {user} = useContext(AuthContext);
+
+
   return (
-    <div>Inbox</div>
+    <>
+      <div>Inbox</div>
+      {{user} ? <p>{`Welcome ${user.nickname}`}</p> : " please log in to see your inbox"}
+    </>
   )
 }
 
