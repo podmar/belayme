d8d95ad9f17adbe0598c0a61820dc1349c0edaeb
podmar <marta.podziewska@gmail.com>
2022-06-29 14:21:19 +0200

    created a custom hook to check if user authenticated

---
+++ client/src/utils/useIsAuthenticated.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { getToken } from './tokenHelpers'
+
+function useIsAuthenticated() {
+    const isAuthenticated = getToken() ? true : false;
+  return isAuthenticated
+}
+
+export default useIsAuthenticated
