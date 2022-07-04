0bb81ad23d4eddcaa7f9a57b3269987c9f77ab19
podmar <marta.podziewska@gmail.com>
2022-07-04 15:21:12 +0200

    reworked the navbar, added button icons

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,69 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import { AuthContext } from '../context/AuthContext';
+import ButtonIconLogin from './ButtonIconLogin';
+import ButtonIconLogout from './ButtonIconLogout';
+import { Grid } from '@mui/material';
 // import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import AddIcon from '@mui/icons-material/Add';
-import SearchIcon from '@mui/icons-material/Search';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import { NavLink } from 'react-router-dom';
+//import IconButton from '@mui/material/IconButton';
+//import SearchIcon from '@mui/icons-material/Search';
+//import MoreIcon from '@mui/icons-material/MoreVert';
+// import { NavLink } from 'react-router-dom';
 
 function NavBar() {
+  const {userLoginStatus} = useContext(AuthContext);
 
   return (
     <>
-      <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+      <AppBar position="fixed" color="primary">
         <Toolbar>
-          <IconButton color="inherit" aria-label="open drawer">
-            <MenuIcon />
-          </IconButton>
-          <Box sx={{ flexGrow: 1 }}>
-            <nav>
-              <ul>
-                <li>
-                  <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/register">Register</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/login">Login</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/list">List</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/profile">Profile</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/inbox">Inbox</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/belayrequest">Message</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/detail/:userid">Detail</NavLink>
-                </li>
-              </ul>
-            </nav>
-
-
-
-
-
-
-
-
-
-          </Box>
-          <IconButton color="inherit">
-            <SearchIcon />
-          </IconButton>
-          <IconButton color="inherit">
-            <MoreIcon />
-          </IconButton>
+          <Grid container justifyContent={"end"}>
+            <Grid item >
+            {userLoginStatus ? <ButtonIconLogout/> : <ButtonIconLogin/>}
+            </Grid>
+              {/* <IconButton color="inherit">
+                <MoreIcon />
+              </IconButton>
+              <IconButton color="inherit">
+                <SearchIcon />
+              </IconButton> */}
+          </Grid>
         </Toolbar>
       </AppBar>
     </>
@@ -71,3 +36,32 @@ function NavBar() {
 }
 
 export default NavBar;
+
+{/* <nav>
+  <ul>
+    <li>
+      <NavLink to="/">Home</NavLink>
+    </li>
+    <li>
+      <NavLink to="/register">Register</NavLink>
+    </li>
+    <li>
+      <NavLink to="/login">Login</NavLink>
+    </li>
+    <li>
+      <NavLink to="/list">List</NavLink>
+    </li>
+    <li>
+      <NavLink to="/profile">Profile</NavLink>
+    </li>
+    <li>
+      <NavLink to="/inbox">Inbox</NavLink>
+    </li>
+    <li>
+      <NavLink to="/belayrequest">Message</NavLink>
+    </li>
+    <li>
+      <NavLink to="/detail/:userid">Detail</NavLink>
+    </li>
+  </ul>
+</nav> */}
