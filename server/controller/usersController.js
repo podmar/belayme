8cb53b6e596d1df73468dd0a4618411ab2fa5819
podmar <marta.podziewska@gmail.com>
2022-07-04 12:22:36 +0200

    removed redundant code

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -196,24 +196,6 @@ const login = async (req, res) => {
     }
 };
 
-// const getProfile = async (req, res) => {
-//     try {
-//         res
-//         .status(200)
-//         .json({
-//             message: "login successful",
-//             user: {
-//                 nickname: req.user.nickname, 
-//                 email: req.user.email, 
-//                 home_crag: req.user.home_crag,
-//             },
-//             });
-        
-//     } catch (error) {
-//         console.log("Cannot get the user profile, server error:", error);
-//     }
-// };
-
 const getUser = (req, res) => {
     console.log("req.user", req.user);
     res
