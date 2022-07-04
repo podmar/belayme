4b1187d7e23d187c831fe8c4b5fee86bee4ece06
podmar <marta.podziewska@gmail.com>
2022-07-04 15:26:32 +0200

    removed redundant comments

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -2,15 +2,10 @@ import { Button, Typography } from '@mui/material';
 import React, { useContext, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import ButtonLogout from "../components/ButtonLogout";
-import UseIsAuthenticated from '../utils/UseIsAuthenticated';
 // import NavBar from '../components/NavBar'
 
 function Home() {
   const navigate = useNavigate();
-  // const [isUserLoggedIn, setIsUserLoggedIn] = useState();
-  // setIsUserLoggedIn(UseIsAuthenticated());
-
-  
 
   const goToRegister = () => {
     navigate("/register")
@@ -18,7 +13,6 @@ function Home() {
 
   return (
     <div>
-    {/* <div className='home-background'> */}
         <Typography variant='h2' className='app-title'>
             belayme
         </Typography>
@@ -33,11 +27,6 @@ function Home() {
         <div>
           <ButtonLogout/>
         </div>
-        {/* <div>
-          {isUserLoggedIn ? "User is currently logged in" : "No user logged in."}
-        </div> */}
-
-        {/* <NavBar/> */}
     </div>
   )
 }
