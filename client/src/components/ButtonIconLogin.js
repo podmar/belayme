6cb53ec3083f62a79a733ec63444779e30c1139e
podmar <marta.podziewska@gmail.com>
2022-07-04 15:42:40 +0200

    changed the loginicon button to button with endicon

--- client/src/components/ButtonIconLogin.js
+++ client/src/components/ButtonIconLogin.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { IconButton } from '@mui/material';
+// import { IconButton, Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import Login from '@mui/icons-material/Login';
 import { useNavigate } from 'react-router-dom';
 
-
 function ButtonIconLogin() {
-    const navigate = useNavigate();
-    const goToLogin = () => navigate("/login");
+  const navigate = useNavigate();
+  const goToLogin = () => navigate("/login");
 
   return (
-    <IconButton
-      color="inherit"
-      size='large'
-      onClick={goToLogin}
-      >
-      <Login/>
-    </IconButton>
+    // <IconButton
+    //   color="inherit"
+    //   size='large'
+    //   onClick={goToLogin}
+    //   >
+    //   <Typography variant='h6'>
+    //     Login
+    //   </Typography>
+    //   <Login/>
+    // </IconButton>
+    <Button
+    color="inherit"
+    size='large'
+    onClick={goToLogin}
+    endIcon={<Login/>}
+    >
+      Login    
+  </Button>
   )
 }
 
