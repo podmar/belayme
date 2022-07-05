e75c350f821f75cc1ce6fd7febd5b08cb15dc461
podmar <marta.podziewska@gmail.com>
2022-07-05 13:40:25 +0200

    created a draft card for the profile

---
+++ client/src/components/CardProfile.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+function CardProfile({profile}) {
+  return (
+    <>
+        <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+            component="img"
+            alt="user profile pic"
+            height="140"
+            // image="xxx.jpg"
+        />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+            {nickname}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+            {about ? about : "Send me a belay request to learn more about me."}
+            </Typography>
+        </CardContent>
+        {/* <CardActions>
+            <Button size="small">Ask for a belay</Button>
+            <Button size="small">See profile</Button>
+        </CardActions> */}
+        </Card>
+    </>
+  )
+}
+
+export default CardUser
