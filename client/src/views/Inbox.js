996add17d97433dc73f5eb06f81ec863d8535f87
podmar <marta.podziewska@gmail.com>
2022-07-12 23:07:46 +0200

    notes on iterating functions to show contacted climbers

--- client/src/views/Inbox.js
+++ client/src/views/Inbox.js
@@ -7,6 +7,43 @@ import ModalAlert from '../components/ModalAlert';
 function Inbox() {
   const {user} = useContext(AuthContext);
 
+  const contactedClimbersIDs = user.sent_requests;
+
+  const searchForClimbers = (id, climberArray) => {
+    let result = [];
+    
+    if (climber._id === id) {
+      result.append(climber);
+    }
+  }
+
+
+
+//   const selectDataFields = (...keys) => {
+//     const getNewClimberObject = (climberObject) => {
+//         const newClimberObject = {};
+//         keys.forEach(key => {
+//             newClimberObject[key] = climberObject[key];
+//         });
+//         return newClimberObject;
+//     }
+//     return getNewClimberObject;
+// };
+
+// const selectContactedClimber = (...ids) => {
+//   const getContactedClimberArray = (climberObject) => {
+//       const newClimberObject = {};
+//       keys.forEach(key => {
+//           newClimberObject[key] = climberObject[key];
+//       });
+//       return newClimberObject;
+//   }
+//   return getNewClimberObject;
+// };
+
+// const allClimbers = allClimbersData
+// .map(selectDataFields("_id", "about", "nickname", "home_crag"));
+
 
   return (
     <>
