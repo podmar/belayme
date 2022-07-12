4deeda56a56196221d5cfedc841730ea26cd62d0
podmar <marta.podziewska@gmail.com>
2022-07-12 12:34:21 +0200

    updated imports

--- client/src/views/ListView.js
+++ client/src/views/ListView.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import List from '../components/List';
-import useIsAuthenticated from '../utils/useIsAuthenticated';
 import ModalAlert from '../components/ModalAlert';
 
 function ListView() {
-  const isUserLoggedIn = useIsAuthenticated();
-
   return (
     <>
       <div>
