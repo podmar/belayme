5ab93f0370f3993d6c9a4d3718f26abb5f0269b7
podmar <marta.podziewska@gmail.com>
2022-07-12 15:22:37 +0200

    removed redundant code

--- client/src/components/ButtonIconLogin.js
+++ client/src/components/ButtonIconLogin.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import { IconButton, Typography } from '@mui/material';
-import { Button, Typography } from '@mui/material';
+import { Button } from '@mui/material';
 import Login from '@mui/icons-material/Login';
 import { useNavigate } from 'react-router-dom';
 
@@ -9,16 +8,6 @@ function ButtonIconLogin() {
   const goToLogin = () => navigate("/login");
 
   return (
-    // <IconButton
-    //   color="inherit"
-    //   size='large'
-    //   onClick={goToLogin}
-    //   >
-    //   <Typography variant='h6'>
-    //     Login
-    //   </Typography>
-    //   <Login/>
-    // </IconButton>
     <Button
     color="inherit"
     size='large'
