9c0c7388d7a7b0d4b1ec7bd8fe10dfbc819c1b30
podmar <marta.podziewska@gmail.com>
2022-08-04 15:16:41 +0200

    added a modal when deleting account to confirm deletion

--- client/src/components/ButtonDeleteProfile.js
+++ client/src/components/ButtonDeleteProfile.js
@@ -1,25 +1,32 @@
-import React, { useContext } from 'react'
-import DeleteIcon from '@mui/icons-material/Delete';
-import Button from '@mui/material/Button';
-import { AuthContext } from '../context/AuthContext';
+import React, { useContext, useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Button from "@mui/material/Button";
+import { AuthContext } from "../context/AuthContext";
+import ModalAlertDeleteAccount from "./ModalAlertDeleteAccount";
 
 function ButtonDeleteProfile() {
-  const {deleteProfile} = useContext(AuthContext);
-
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
+  const handleCloseDeleteModal = () => {
+    setOpenDeleteModal(false);
+  };
   //TODO make this button display a modal to delete account to make sure the user does not delete by accident
   return (
     <div>
-        <Button
-            // type='submit'
-            endIcon={<DeleteIcon />}
-            variant='outline'
-            // color='primary'
-            onClick={deleteProfile}
-        >
-            Delete account
-        </Button>
+      <Button
+        // type='submit'
+        endIcon={<DeleteIcon />}
+        variant="outline"
+        // color='primary'
+        onClick={() => setOpenDeleteModal(true)}
+      >
+        Delete account
+      </Button>
+      <ModalAlertDeleteAccount
+        open={openDeleteModal}
+        close={handleCloseDeleteModal}
+      />
     </div>
-  )
+  );
 }
 
 export default ButtonDeleteProfile;
---
+++ client/src/components/ModalAlertDeleteAccount.js
@@ -0,0 +1,66 @@
+import { Alert, Box, Typography, Modal, Button } from "@mui/material";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+
+function ModalAlertDeleteAccount({ open, close }) {
+  const { deleteProfile } = useContext(AuthContext);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  };
+
+  return (
+    <div>
+      <Modal open={open} onClose={close} aria-labelledby="warning">
+        <Box sx={style}>
+          <Alert severity="warning">
+            <Box>
+              <Box sx={{ pb: 1 }}>
+                Deleting your account is irreversible. Are you sure you want to
+                proceed?
+              </Box>
+              <Box
+                sx={{
+                  dispay: "flex",
+                  gap: 2,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Box sx={{ py: 1 }}>
+                  <Button
+                    fullWidth
+                    startIcon={<ArrowBackIosIcon />}
+                    variant="contained"
+                    color="primary"
+                    onClick={close}
+                  >
+                    No, go back!
+                  </Button>
+                </Box>
+                <Box sx={{ py: 1 }}>
+                  <Button
+                    fullWidth
+                    endIcon={<DeleteIcon />}
+                    variant="contained"
+                    color="secondary"
+                    onClick={deleteProfile}
+                  >
+                    Yes, delete
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
+          </Alert>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+export default ModalAlertDeleteAccount;
