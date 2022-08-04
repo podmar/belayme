9f8f4312ef6d9d95b4419552a7a9fc19c31194e9
podmar <marta.podziewska@gmail.com>
2022-08-04 11:01:41 +0200

    removed commented redundant code about conditional background

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,91 @@
-import React from 'react'
-import './App.css';
-import { Routes, Route, useLocation} from 'react-router-dom';
-import "@fontsource/roboto"
+import React from "react";
+import "./App.css";
+import { Routes, Route, useLocation } from "react-router-dom";
+import "@fontsource/roboto";
 import CssBaseline from "@mui/material/CssBaseline";
-import {Container} from "@mui/material"
-import { ThemeProvider, createTheme } from "@mui/material/styles"
-import { themeLightSettings } from './utils/muiThemesSettings';
-import Home from './views/Home';
-import Login from './views/Login';
-import Register from './views/Register';
-import MyProfile from './views/MyProfile';
-import ListView from './views/ListView';
-import DetailPartnerView from './views/DetailPartnerView';
-import ErrorPage from './views/ErrorPage';
-import BelayRequest from './views/BelayRequest';
-import Inbox from './views/Inbox';
-import NavBottomWithLinks from './components/NavBottomWithLinks';
-import { ClimbersContextProvider } from './context/ClimbersContext';
-import { AuthContextProvider } from './context/AuthContext';
-import NavBar from './components/NavBar';
-import ProtectedRoute from './components/ProtectedRoute';
-
+import { Container } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { themeLightSettings } from "./utils/muiThemesSettings";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import MyProfile from "./views/MyProfile";
+import ListView from "./views/ListView";
+import DetailPartnerView from "./views/DetailPartnerView";
+import ErrorPage from "./views/ErrorPage";
+import BelayRequest from "./views/BelayRequest";
+import Inbox from "./views/Inbox";
+import NavBottomWithLinks from "./components/NavBottomWithLinks";
+import { ClimbersContextProvider } from "./context/ClimbersContext";
+import { AuthContextProvider } from "./context/AuthContext";
+import NavBar from "./components/NavBar";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const location = useLocation();
 
-  // const addBackground = () => {
-  //   console.log(location.pathname);
-  //   if (location.pathname === "/" || location.pathname === "/register" || location.pathname === "/login" ) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // } 
-
-  // useEffect(() => {
-  //   addBackground()
-  // }, [location])
-  
-    const themeLight = createTheme (themeLightSettings);
+  const themeLight = createTheme(themeLightSettings);
 
   return (
-    <ThemeProvider theme= {themeLight}>
+    <ThemeProvider theme={themeLight}>
       <CssBaseline />
       <Container maxWidth="md">
-      <AuthContextProvider>
-        <ClimbersContextProvider>
-          <div className="App">
-            <header>
-              <NavBar/>
-              {/* TODO create a state variable for the view where the user is and create conditional statements for different variation of the NavBar */}
-              <NavBottomWithLinks/>
-            </header>
+        <AuthContextProvider>
+          <ClimbersContextProvider>
+            <div className="App">
+              <header>
+                <NavBar />
+                <NavBottomWithLinks />
+              </header>
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="login" element={<Login />} />
-                <Route path="list" element={<ProtectedRoute><ListView /></ProtectedRoute>} />
-                <Route path="belayrequest" element={<ProtectedRoute><BelayRequest /></ProtectedRoute>} />
-                <Route path="detail/:userid" element={<ProtectedRoute><DetailPartnerView /></ProtectedRoute>} />
-                <Route path="inbox" element={<ProtectedRoute><Inbox /></ProtectedRoute>} />
-                <Route path="profile" element={<ProtectedRoute><MyProfile /></ProtectedRoute>} />
+                <Route
+                  path="list"
+                  element={
+                    <ProtectedRoute>
+                      <ListView />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="belayrequest"
+                  element={
+                    <ProtectedRoute>
+                      <BelayRequest />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="detail/:userid"
+                  element={
+                    <ProtectedRoute>
+                      <DetailPartnerView />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="inbox"
+                  element={
+                    <ProtectedRoute>
+                      <Inbox />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="profile"
+                  element={
+                    <ProtectedRoute>
+                      <MyProfile />
+                    </ProtectedRoute>
+                  }
+                />
                 <Route path="*" element={<ErrorPage />} />
               </Routes>
-          </div>
-        </ClimbersContextProvider>
-      </AuthContextProvider>
+            </div>
+          </ClimbersContextProvider>
+        </AuthContextProvider>
       </Container>
     </ThemeProvider>
   );
