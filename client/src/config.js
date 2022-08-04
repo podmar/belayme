f2a97d88adde25e6b72f7d4489154180c6b6dce6
podmar <marta.podziewska@gmail.com>
2022-08-04 15:15:05 +0200

    fixed a typo in development localhost address

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,6 @@
-const serverURL = process.env.NODE_ENV === "development" 
-    ? "http://localhost:5001" 
-    : "https://belay-me.herokuapp.com/"
+const serverURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5001/"
+    : "https://belay-me.herokuapp.com/";
 
 export default serverURL;
