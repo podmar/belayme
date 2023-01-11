a9d529fad91728de031c49177c0585c0073293b9
podmar <marta.podziewska@gmail.com>
2023-01-11 15:35:59 +0100

    removed cors options for vercel deployment

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import usersRoute from "./routes/usersRoute.js"
 import climbersRoute from "./routes/climbersRoute.js"
 import * as dotenv from "dotenv";
-import mongoose from "mongoose"; 
+import mongoose from "mongoose";
 import passport from "passport";
 import passportConfig from "./config/passport.js";
 import { cloudinaryConfig } from "./config/cloudinary.js";
@@ -13,19 +13,20 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5001;
-const allowedDomains = ["https://belayme.netlify.app", "http://localhost:3000"]
-const corsOptions = {
-    origin: function (origin,callback) {
-        if (!origin) return callback(null, true);
- 
-        if (allowedDomains.indexOf(origin) === -1) {
-            const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-            return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-    }, 
-    credentials: true,
-}; 
+// removing cors options for vercel deployment:
+// const allowedDomains = ["https://belayme.netlify.app", "http://localhost:3000"]
+// const corsOptions = {
+//     origin: function (origin,callback) {
+//         if (!origin) return callback(null, true);
+
+//         if (allowedDomains.indexOf(origin) === -1) {
+//             const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+//             return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//     },
+//     credentials: true,
+// };
 
 app.use(express.json());
 app.use(
@@ -33,7 +34,8 @@ app.use(
         extended: true,
     })
 );
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions)); // removing for vercel deployment
+app.use(cors());
 app.use('/users', usersRoute);
 app.use('/climbers', climbersRoute);
 app.use(passport.initialize());
@@ -46,5 +48,5 @@ mongoose
     .catch(err => console.log(err));
 
 app.listen(port, () => {
-    console.log(`Server is running on the ${port} port.`); 
+    console.log(`Server is running on the ${port} port.`);
 });
