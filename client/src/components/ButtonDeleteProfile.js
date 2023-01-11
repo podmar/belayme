a7afd5f5a98bd90e4f26d4cef22abe6cf0f01712
podmar <marta.podziewska@gmail.com>
2023-01-11 13:49:16 +0100

    fixing imports

--- client/src/components/ButtonDeleteProfile.js
+++ client/src/components/ButtonDeleteProfile.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Button from "@mui/material/Button";
 import ModalAlertDeleteAccount from "./ModalAlertDeleteAccount";
--- client/src/components/CardClimber.js
+++ client/src/components/CardClimber.js
@@ -1,4 +1,4 @@
-import { React, useContext } from "react";
+import React, { useContext } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
