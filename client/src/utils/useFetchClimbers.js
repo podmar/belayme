e5faa55b5278f9fea70b2759c2d7f51881e72fe3
podmar <marta.podziewska@gmail.com>
2023-01-17 14:36:47 +0100

    added loading status

--- client/src/utils/useFetchClimbers.js
+++ client/src/utils/useFetchClimbers.js
@@ -4,21 +4,36 @@ import serverURL from "../config";
 export const useFetchClimbers = () => {
   const [climbers, setClimbers] = useState(null);
   const [climberCount, setClimberCount] = useState([0]);
+  const [loadingStatus, setLoadingStatus] = useState("idle");
 
   const fetchClimbers = async () => {
+    setLoadingStatus("loading");
     try {
       const response = await fetch(serverURL + "climbers/all");
       const climbersData = await response.json();
       setClimbers(climbersData.allClimbers);
       setClimberCount(climbersData.results);
+      setLoadingStatus("loaded");
       console.log(climbersData);
     } catch (error) {
-      console.log("cannot fetch climbers", error);
+      setLoadingStatus("errored");
+      console.log("Fetching climbers failed.", error);
     }
   };
 
   useEffect(() => {
     fetchClimbers();
   }, []);
-  return { climbers, setClimbers, climberCount, setClimberCount };
+
+  useEffect(() => {
+    console.log(loadingStatus);
+  }, [loadingStatus]);
+
+  return {
+    climbers,
+    setClimbers,
+    climberCount,
+    setClimberCount,
+    loadingStatus,
+  };
 };
