ba7e44452500cf5bba5cec02c9cbd899b9e7d3a0
podmar <marta.podziewska@gmail.com>
2023-01-17 13:43:49 +0100

    formatting change by prettier

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,31 +1,26 @@
-import { Typography } from '@mui/material';
-import React, { useContext } from 'react'
-import ClimbersContext from '../context/ClimbersContext';
-import CardClimber from './CardClimber';
+import { Typography } from "@mui/material";
+import React, { useContext } from "react";
+import ClimbersContext from "../context/ClimbersContext";
+import CardClimber from "./CardClimber";
 
 function List() {
-    const { climbers, climberCount } = useContext(ClimbersContext);
-    
-    //TODO change this to get user by location based on current location
+  const { climbers, climberCount } = useContext(ClimbersContext);
 
-    return (
+  //TODO change this to get user by location based on current location
+
+  return (
     <div>
-        <Typography variant='body2'>
-            {climberCount ? 
-                `Found ${climberCount} people nearby you.` :
-                "Looking for people nearby..."
-            }
-        </Typography>          
-            {climbers && climbers.map(climber => {
-                return (
-                        <CardClimber 
-                        climber={climber}
-                        key={climber._id}
-                        />
-                )
-            })}
+      <Typography variant="body2">
+        {climberCount
+          ? `Found ${climberCount} people nearby you.`
+          : "Looking for people nearby..."}
+      </Typography>
+      {climbers &&
+        climbers.map((climber) => {
+          return <CardClimber climber={climber} key={climber._id} />;
+        })}
     </div>
-  )
+  );
 }
 
 export default List;
