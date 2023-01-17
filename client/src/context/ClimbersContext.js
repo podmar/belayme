e1e017c73018249e2582303ab622397142b3612f
podmar <marta.podziewska@gmail.com>
2023-01-17 14:08:41 +0100

    applied the fetch climbers hook and cleaned up redundant notes

--- client/src/context/ClimbersContext.js
+++ client/src/context/ClimbersContext.js
@@ -1,14 +1,28 @@
-import React, { createContext, useEffect, useMemo, useState } from "react";
-import serverURL from "../config";
-import { useFetch } from "../utils/useFetch";
+import React, { createContext } from "react";
+import { useFetchClimbers } from "../utils/useFetchClimbers";
 
 export const ClimbersContext = createContext();
 export const ClimbersContextProvider = (props) => {
-  const [climbers, setClimbers] = useState(null);
-  const [climberCount, setClimberCount] = useState([0]);
-  // const [singleClimber, setSingleClimber] = useState(null);
+  //TODO change this to get climbers by location based on current location / browser location / location search by user (add query / url to the custom hook)
 
-  //TODO change this to get climbers by location based on current location / browser location / location search by user
+  const {
+    climbers,
+    setClimbers,
+    climberCount,
+    setClimberCount,
+  } = useFetchClimbers();
+
+  //TODO create functionality to fetch a single climber and move it to relevant view / component
+  // const [singleClimber, setSingleClimber] = useState(null);
+  // const fetchSingleClimber = async (climberID) => {
+  //     try {
+  //         const response = await fetch(serverURL+"climbers/"+climberID);
+  //         const singleClimberData = await response.json();
+  //         setSingleClimber(singleClimberData.allClimbers);
+  //     } catch (error) {
+  //         console.log("cannot fetch this climber", error);
+  //     }
+  // };
 
   // const [query, setQuery] = useState("");
   // const url = query && `${serverURL}climbers/all${query}`;
@@ -21,32 +35,6 @@ export const ClimbersContextProvider = (props) => {
   // console.log(fetchedData);
   // setClimbers(fetchedData.allClimbers);
   // setClimberCount(fetchedData.results);
-
-  const fetchClimbers = async () => {
-    try {
-        const response = await fetch(serverURL+"climbers/all");
-        const climbersData = await response.json();
-        setClimbers(climbersData.allClimbers);
-        setClimberCount(climbersData.results);
-        console.log(climbersData);
-    } catch (error) {
-        console.log("cannot fetch climbers", error);
-    }
-};
-
-  //   const fetchSingleClimber = async (climberID) => {
-  //     try {
-  //         const response = await fetch(serverURL+"climbers/"+climberID);
-  //         const singleClimberData = await response.json();
-  //         setSingleClimber(singleClimberData.allClimbers);
-  //     } catch (error) {
-  //         console.log("cannot fetch this climber", error);
-  //     }
-  // };
-
-  useEffect(() => {
-    fetchClimbers();
-  }, []);
 
   return (
     <ClimbersContext.Provider
