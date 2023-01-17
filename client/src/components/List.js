d7ae23b206155f2447a600fe86e78c8d1db84dc6
podmar <marta.podziewska@gmail.com>
2023-01-17 17:00:46 +0100

    applied logics checking if user logged in and 2 different cards

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,10 +1,19 @@
 import { Typography } from "@mui/material";
 import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 import ClimbersContext from "../context/ClimbersContext";
 import CardClimber from "./CardClimber";
+import CardClimberLoggedOut from "./CardClimberLoggedOut";
 
 function List() {
   const { climbers, climberCount } = useContext(ClimbersContext);
+  const { user } = useContext(AuthContext);
+  let climbersToDisplay = null;
+
+  //TODO if refreshing on a list view, the list breaks (climber or user misssing values?)
+  if (user && climbers) {
+    climbersToDisplay = climbers.filter((climber) => climber._id !== user._id);
+  }
 
   //TODO change this to get user by location based on current location
 
@@ -15,10 +24,13 @@ function List() {
           ? `Found ${climberCount} people nearby you.`
           : "Looking for people nearby..."}
       </Typography>
-      {climbers &&
-        climbers.map((climber) => {
-          return <CardClimber climber={climber} key={climber._id} />;
-        })}
+      {climbersToDisplay
+        ? climbersToDisplay.map((climber) => {
+            return <CardClimber climber={climber} key={climber._id} />;
+          })
+        : climbers.map((climber) => {
+            return <CardClimberLoggedOut climber={climber} key={climber._id} />;
+          })}
     </div>
   );
 }
