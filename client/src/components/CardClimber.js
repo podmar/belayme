44174fda847baf72965d4aaa04ed3ffa4fe69bcd
podmar <marta.podziewska@gmail.com>
2023-01-17 16:53:22 +0100

    removed the logic checking if user logged in

--- client/src/components/CardClimber.js
+++ client/src/components/CardClimber.js
@@ -7,11 +7,11 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Box, Chip, Stack } from "@mui/material";
 import ButtonBelayRequest from "./ButtonBelayRequest";
-import { AuthContext } from "../context/AuthContext";
+// import { AuthContext } from "../context/AuthContext";
 // import ButtonIconBelayRequest from "./ButtonIconBelayRequest";
 
 function CardClimber({ climber }) {
-  const { user } = useContext(AuthContext);
+  // const { user } = useContext(AuthContext);
 
   // const checkIfRequestSent = () => {
   //   if (user.sent_requests) {
@@ -26,100 +26,94 @@ function CardClimber({ climber }) {
   //   checkIfRequestSent()
   // }, [user])
 
-  if (user) {
-    if (climber._id === user._id) {
-      return;
-    } else {
-      return (
-        <Box className="belayme-custom-box-center" py={1}>
-          <Card
-            sx={{
-              width: "90%",
-            }}
+  return (
+    <Box className="belayme-custom-box-center" py={1}>
+      <Card
+        sx={{
+          width: "90%",
+        }}
+      >
+        {climber.image && (
+          <CardMedia
+            component="img"
+            alt={`Photo of ${climber.nickname}`}
+            height="300"
+            image={climber.image}
+          />
+        )}
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {climber.nickname}
+          </Typography>
+          <div className="padding-y-05">
+            <Typography variant="body2" color="text.secondary">
+              {climber.about
+                ? climber.about
+                : "Send me a belay request to learn more about me."}
+            </Typography>
+          </div>
+          <Stack
+            direction="row"
+            alignItems={"space-between"}
+            spacing={1}
+            flexWrap={"wrap"}
+            xs={{ p: 0.5, m: 0 }}
           >
-            {climber.image && (
-              <CardMedia
-                component="img"
-                alt={`Photo of ${climber.nickname}`}
-                height="300"
-                image={climber.image}
-              />
-            )}
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {climber.nickname}
-              </Typography>
-              <div className="padding-y-05">
-                <Typography variant="body2" color="text.secondary">
-                  {climber.about
-                    ? climber.about
-                    : "Send me a belay request to learn more about me."}
-                </Typography>
+            {climber.experience_y && (
+              <div className="padding-y-03">
+                <Chip
+                  color="secondary"
+                  label={`climbing ${climber.experience_y} years`}
+                ></Chip>
               </div>
-              <Stack
-                direction="row"
-                alignItems={"space-between"}
-                spacing={1}
-                flexWrap={"wrap"}
-                xs={{ p: 0.5, m: 0 }}
-              >
-                {climber.experience_y && (
-                  <div className="padding-y-03">
-                    <Chip
-                      color="secondary"
-                      label={`climbing ${climber.experience_y} years`}
-                    ></Chip>
-                  </div>
-                )}
-                {climber.onsight_level && (
-                  <div className="padding-y-03">
-                    <Chip
-                      color="primary"
-                      label={`onsights ${climber.onsight_level}`}
-                    ></Chip>
-                  </div>
-                )}
-                {climber.current_location && (
-                  <div className="padding-y-03">
-                    <Chip
-                      color="primary"
-                      label={`${climber.current_location}`}
-                    ></Chip>
-                  </div>
-                )}
-                {climber.weight && (
-                  <div className="padding-y-03">
-                    <Chip color="primary" label={`${climber.weight} kg`}></Chip>
-                  </div>
-                )}
-                {/* TODO Check why style is not dissappearing when empty */}
-                {climber.climbing_style.length !== 0 && (
-                  <div className="padding-y-03">
-                    <Chip
-                      color="primary"
-                      label={`${climber.climbing_style}`}
-                    ></Chip>
-                  </div>
-                )}
-                {/* {climber. &&
+            )}
+            {climber.onsight_level && (
+              <div className="padding-y-03">
+                <Chip
+                  color="primary"
+                  label={`onsights ${climber.onsight_level}`}
+                ></Chip>
+              </div>
+            )}
+            {climber.current_location && (
+              <div className="padding-y-03">
+                <Chip
+                  color="primary"
+                  label={`${climber.current_location}`}
+                ></Chip>
+              </div>
+            )}
+            {climber.weight && (
+              <div className="padding-y-03">
+                <Chip color="primary" label={`${climber.weight} kg`}></Chip>
+              </div>
+            )}
+            {/* TODO Check why style is not dissappearing when empty */}
+            {climber.climbing_style.length !== 0 && (
+              <div className="padding-y-03">
+                <Chip
+                  color="primary"
+                  label={`${climber.climbing_style}`}
+                ></Chip>
+              </div>
+            )}
+            {/* {climber. &&
                 <Chip
                 color="primary"
                 label={`${climber.}`}
                 ></Chip>} */}
-              </Stack>
-            </CardContent>
-            <CardActions>
-              {/* {checkIfRequestSent && <ButtonBelayRequest id={climber._id}/>} */}
-              <ButtonBelayRequest id={climber._id} />
-              {/* <Button size="small">Ask for a belay</Button> */}
-              {/* <ButtonIconBelayRequest id={climber._id}/> */}
-              <Button size="small">See profile</Button>
-            </CardActions>
-          </Card>
-        </Box>
-      );
-    }
-  }
+          </Stack>
+        </CardContent>
+        <CardActions>
+          {/* {checkIfRequestSent && <ButtonBelayRequest id={climber._id}/>} */}
+          <ButtonBelayRequest id={climber._id} />
+          {/* <Button size="small">Ask for a belay</Button> */}
+          {/* <ButtonIconBelayRequest id={climber._id}/> */}
+          <Button size="small">See profile</Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
 }
 
 export default CardClimber;
