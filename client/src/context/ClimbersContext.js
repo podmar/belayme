76c44cc314f11783fee05eae8587f374cb31d9b7
podmar <marta.podziewska@gmail.com>
2023-01-17 15:28:29 +0100

    applied the useFetch hooks

--- client/src/context/ClimbersContext.js
+++ client/src/context/ClimbersContext.js
@@ -1,16 +1,32 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import serverURL from "../config";
+import { useFetch } from "../utils/useFetch";
 import { useFetchClimbers } from "../utils/useFetchClimbers";
 
 export const ClimbersContext = createContext();
 export const ClimbersContextProvider = (props) => {
   //TODO change this to get climbers by location based on current location / browser location / location search by user (add query / url to the custom hook)
 
-  const {
-    climbers,
-    setClimbers,
-    climberCount,
-    setClimberCount,
-  } = useFetchClimbers();
+  const allClimbersURL = `${serverURL}climbers/all`;
+
+  // const {
+  //   climbers,
+  //   setClimbers,
+  //   climberCount,
+  //   setClimberCount,
+  // } = useFetchClimbers(allClimbersURL);
+
+  // Using the general useFetch hook
+  const [climbers, setClimbers] = useState(null);
+  const [climberCount, setClimberCount] = useState([0]);
+  const { fetchedData, loadingStatus } = useFetch(allClimbersURL);
+
+  useEffect(() => {
+    if (loadingStatus === "loaded") {
+      setClimbers(fetchedData.allClimbers);
+      setClimberCount(fetchedData.results);
+    }
+  }, [loadingStatus]);
 
   //TODO create functionality to fetch a single climber and move it to relevant view / component
   // const [singleClimber, setSingleClimber] = useState(null);
