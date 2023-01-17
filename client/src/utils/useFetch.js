b31c3b191a5bf6f4dc2e0d3e3f21de8b65aab434
podmar <marta.podziewska@gmail.com>
2023-01-17 15:25:17 +0100

    rewrote useFetch following useFetchClimbers pattern

--- client/src/utils/useFetch.js
+++ client/src/utils/useFetch.js
@@ -1,29 +1,59 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useFetch = (url) => {
-  const [fetchStatus, setFetchStatus] = useState("idle");
-  const [fetchedData, setFetchedData] = useState({});
+  const [fetchedData, setFetchedData] = useState(null);
+  const [loadingStatus, setLoadingStatus] = useState("idle");
 
-  const fetchData = useCallback(async () => {
-    setFetchStatus("loading");
-    console.log(fetchStatus);
+  const fetchData = async () => {
+    setLoadingStatus("loading");
     try {
       const responce = await fetch(url);
       const data = await responce.json();
-      //   setFetchedData({ data: data, loading: false });
       setFetchedData(data);
-      setFetchStatus("loaded");
-      console.log(fetchStatus);
+      setLoadingStatus("loaded");
     } catch (error) {
-      setFetchStatus("errored");
+      setLoadingStatus("errored");
       console.log("Fetching data failed.", error);
-      console.log(fetchStatus);
     }
-  }, [url])
+  };
 
   useEffect(() => {
     fetchData();
-  }, [url]);
+  }, []);
 
-  return { fetchedData, fetchStatus };
+  useEffect(() => {
+    console.log(loadingStatus);
+  }, [loadingStatus]);
+
+  return { fetchedData, loadingStatus };
 };
+
+// import { useCallback, useEffect, useState } from "react";
+
+// export const useFetch = (url) => {
+//   const [loadingStatus, setLoadingStatus] = useState("idle");
+//   const [fetchedData, setFetchedData] = useState({});
+
+//   const fetchData = useCallback(async () => {
+//     setLoadingStatus("loading");
+//     console.log(loadingStatus);
+//     try {
+//       const responce = await fetch(url);
+//       const data = await responce.json();
+//       //   setFetchedData({ data: data, loading: false });
+//       setFetchedData(data);
+//       setLoadingStatus("loaded");
+//       console.log(loadingStatus);
+//     } catch (error) {
+//       setLoadingStatus("errored");
+//       console.log("Fetching data failed.", error);
+//       console.log(loadingStatus);
+//     }
+//   }, [url]);
+
+//   useEffect(() => {
+//     fetchData();
+//   }, [url]);
+
+//   return { fetchedData, loadingStatus };
+// };
