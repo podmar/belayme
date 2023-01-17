865f8ef71cde290f34d49cff363ab2c1115bb342
podmar <marta.podziewska@gmail.com>
2023-01-17 13:45:08 +0100

    added use callback and use memo: still not functional, too many rerenders

--- client/src/context/ClimbersContext.js
+++ client/src/context/ClimbersContext.js
@@ -1,5 +1,6 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useEffect, useMemo, useState } from "react";
 import serverURL from "../config";
+import { useFetch } from "../utils/useFetch";
 
 export const ClimbersContext = createContext();
 export const ClimbersContextProvider = (props) => {
@@ -7,42 +8,53 @@ export const ClimbersContextProvider = (props) => {
   const [climberCount, setClimberCount] = useState([0]);
   // const [singleClimber, setSingleClimber] = useState(null);
 
-
   //TODO change this to get climbers by location based on current location / browser location / location search by user
 
-  const fetchClimbers = async () => {
-      try {
-          const response = await fetch(serverURL+"climbers/all");
-          const climbersData = await response.json();
-          setClimbers(climbersData.allClimbers);
-          setClimberCount(climbersData.results);
-          console.log(climbersData);
-      } catch (error) {
-          console.log("cannot fetch climbers", error);
-      }
-  };
+  // const [query, setQuery] = useState("");
+  // const url = query && `${serverURL}climbers/all${query}`;
+  // const url = useMemo(() => `${serverURL}climbers/all`, [])
 
-//   const fetchSingleClimber = async (climberID) => {
-//     try {
-//         const response = await fetch(serverURL+"climbers/"+climberID);
-//         const singleClimberData = await response.json();
-//         setSingleClimber(singleClimberData.allClimbers);
-//     } catch (error) {
-//         console.log("cannot fetch this climber", error);
-//     }
-// };
+  // // const [url, setUrl] = useState(`${serverURL}climbers/all`);
+
+  // // console.log("This is the URL: " + url);
+  // const { fetchedData, fetchStatus } = useFetch(url);
+  // console.log(fetchedData);
+  // setClimbers(fetchedData.allClimbers);
+  // setClimberCount(fetchedData.results);
+
+  const fetchClimbers = async () => {
+    try {
+        const response = await fetch(serverURL+"climbers/all");
+        const climbersData = await response.json();
+        setClimbers(climbersData.allClimbers);
+        setClimberCount(climbersData.results);
+        console.log(climbersData);
+    } catch (error) {
+        console.log("cannot fetch climbers", error);
+    }
+};
+
+  //   const fetchSingleClimber = async (climberID) => {
+  //     try {
+  //         const response = await fetch(serverURL+"climbers/"+climberID);
+  //         const singleClimberData = await response.json();
+  //         setSingleClimber(singleClimberData.allClimbers);
+  //     } catch (error) {
+  //         console.log("cannot fetch this climber", error);
+  //     }
+  // };
 
   useEffect(() => {
-    fetchClimbers()
-  }, [])
+    fetchClimbers();
+  }, []);
 
   return (
     <ClimbersContext.Provider
-    value = {{climbers, setClimbers, climberCount, setClimberCount}}
+      value={{ climbers, setClimbers, climberCount, setClimberCount }}
     >
       {props.children}
     </ClimbersContext.Provider>
-  )
-}
+  );
+};
 
-export default ClimbersContext
+export default ClimbersContext;
--- client/src/utils/useFetch.js
+++ client/src/utils/useFetch.js
@@ -1,31 +1,29 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const useFetch = (url) => {
   const [fetchStatus, setFetchStatus] = useState("idle");
   const [fetchedData, setFetchedData] = useState({});
 
-  useEffect(() => {
-    // setFetchedData((fetchedData) => ({ data: fetchData.data, loading: true }));
-    if (!url) return;
+  const fetchData = useCallback(async () => {
+    setFetchStatus("loading");
     console.log(fetchStatus);
-
-    const fetchData = async () => {
-      setFetchStatus("loading");
+    try {
+      const responce = await fetch(url);
+      const data = await responce.json();
+      //   setFetchedData({ data: data, loading: false });
+      setFetchedData(data);
+      setFetchStatus("loaded");
       console.log(fetchStatus);
-      try {
-        const responce = await fetch(url);
-        const data = await responce.json();
-        //   setFetchedData({ data: data, loading: false });
-        setFetchedData(data);
-        setFetchStatus("complete");
-        console.log(fetchStatus);
-      } catch (error) {
-        setFetchStatus("failed");
-        console.log("Fetching data failed.", error);
-        console.log(fetchStatus);
-      }
-    };
+    } catch (error) {
+      setFetchStatus("errored");
+      console.log("Fetching data failed.", error);
+      console.log(fetchStatus);
+    }
+  }, [url])
+
+  useEffect(() => {
     fetchData();
   }, [url]);
+
   return { fetchedData, fetchStatus };
 };
