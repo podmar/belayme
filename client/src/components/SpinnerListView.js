72493e865712cc989de440c6ede400f8eeaf62ba
podmar <marta.podziewska@gmail.com>
2023-01-20 18:20:37 +0100

    extracted a spinner component

---
+++ client/src/components/SpinnerListView.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { CircularProgress, Typography } from "@mui/material";
+
+export const SpinnerListView = () => {
+  return (
+    <div className="list-view-spacing">
+      <CircularProgress />
+      <Typography variant="body2">"Looking for people nearby..."</Typography>
+    </div>
+  );
+};
--- client/src/views/ListView.js
+++ client/src/views/ListView.js
@@ -1,25 +1,14 @@
-import { CircularProgress, Typography } from "@mui/material";
 import React, { useContext } from "react";
 import List from "../components/List";
 import ModalAlert from "../components/ModalAlert";
 import ClimbersContext from "../context/ClimbersContext";
+import { SpinnerListView } from "../components/SpinnerListView";
 
 function ListView() {
   const { climbers } = useContext(ClimbersContext);
   return (
     <>
-      <div>
-        {climbers ? (
-          <List />
-        ) : (
-          <div className="list-view-spacing">
-            <CircularProgress />
-            <Typography variant="body2">
-              "Looking for people nearby..."
-            </Typography>
-          </div>
-        )}
-      </div>
+      <div>{climbers ? <List /> : <SpinnerListView />}</div>
       <ModalAlert />
     </>
   );
