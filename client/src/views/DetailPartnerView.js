42cc2f321633aaef2190e366f4baa31d51d927e4
podmar <marta.podziewska@gmail.com>
2023-01-26 17:11:03 +0100

    used the detail climber card, useparams for climberid and created climber state variable

--- client/src/views/DetailPartnerView.js
+++ client/src/views/DetailPartnerView.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ModalAlert from '../components/ModalAlert';
-
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import CardClimberDetail from "../components/CardClimberDetail";
+import ModalAlert from "../components/ModalAlert";
 
 function DetailPartnerView() {
-  return (
-    <div>DetailPartnerView
-      <ModalAlert/>
+  const { climberid } = useParams();
+  const [climber, setClimber] = useState(null);
 
+  return (
+    <div>
+      <h6>DetailPartnerView</h6>
+      {climber && <CardClimberDetail climber={climber} />}
+      <ModalAlert />
     </div>
-  )
+  );
 }
 
-export default DetailPartnerView
+export default DetailPartnerView;
