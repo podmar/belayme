8e50bc3a60de8dadf98b702dcf9dbc1951adc257
podmar <marta.podziewska@gmail.com>
2023-01-26 18:25:26 +0100

    fetched single climber and updated the climber detail state variable

--- client/src/views/DetailPartnerView.js
+++ client/src/views/DetailPartnerView.js
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CardClimberDetail from "../components/CardClimberDetail";
 import ModalAlert from "../components/ModalAlert";
+import { useFetch } from "../utils/useFetch";
 
 function DetailPartnerView() {
   const { climberid } = useParams();
-  const [climber, setClimber] = useState(null);
+  const [climberDetail, setClimberDetail] = useState(null);
+
+  const { fetchedData, loadingStatus } = useFetch(`climbers/id/${climberid}`);
+
+  useEffect(() => {
+    if (loadingStatus === "loaded") {
+      console.log(fetchedData);
+      setClimberDetail(fetchedData.climber);
+    }
+  }, [loadingStatus]);
 
   return (
     <div>
-      <h6>DetailPartnerView</h6>
-      {climber && <CardClimberDetail climber={climber} />}
+      <h6>Detail Partner View</h6>
+      {climberDetail && <CardClimberDetail climber={climberDetail} />}
       <ModalAlert />
     </div>
   );
